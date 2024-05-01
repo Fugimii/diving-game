a3dfe18b6eb7b1b1f532845738d506cf141b01cf
Fugimii <louisrumbleskins@gmail.com>
2024-05-01 15:50:48 +1000

    yay

--- scenes/generation/chunk/chunk.gd
+++ scenes/generation/chunk/chunk.gd
@@ -1,18 +1,35 @@
+@tool
 extends Node2D
 
-@export var noise : Noise
-@export var noise_threshold = 0.5
-@export var width = 64
-@export var height = 64
+@export var noise : Noise :
+	set(value):
+		noise = value
+		generate()
+
+@export var noise_threshold = 0.5 :
+	set(value):
+		noise_threshold = value
+		generate()
+
+@export var width = 64 :
+	set(value):
+		width = value
+		generate()
+
+@export var height = 64 :
+	set(value):
+		height = value
+		generate()
 
 @onready var tilemap = $TileMap
 
-func _ready():
-	tilemap.set_cell(0, Vector2i(0, 0), 0, Vector2i.ZERO, 0)
-	for x in range(width):
-		for y in range(height):
-			var noise_height = noise.get_noise_2d(x, y)
-			
-			if noise_height > noise_threshold:
-				print("HI :D")
-				tilemap.set_cell(0, Vector2i(x, y), 0, Vector2i.ZERO, 0)
+func generate():
+	if tilemap:
+		tilemap.clear()
+		tilemap.set_cell(0, Vector2i(0, 0), 2, Vector2i.ZERO, 0)
+		for x in range(width):
+			for y in range(height):
+				var noise_height = noise.get_noise_2d(x, y)
+				
+				if noise_height > noise_threshold:
+					tilemap.set_cell(0, Vector2i(x, y), 2, Vector2i.ZERO, 0)
